ff058503ba9741e3cf10398e894d36504c868452
dorayo <yomingyo@gmail.com>
2019-02-27 23:11:55 +0800

    Initial commit

---
+++ Example/GMBaseThirdSDK/GMAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  GMAppDelegate.h
+//  GMBaseThirdSDK
+//
+//  Created by ioszhanghui@163.com on 02/27/2019.
+//  Copyright (c) 2019 ioszhanghui@163.com. All rights reserved.
+//
+
+@import UIKit;
+
+@interface GMAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Example/GMBaseThirdSDK/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  GMBaseThirdSDK
+//
+//  Created by ioszhanghui@163.com on 02/27/2019.
+//  Copyright (c) 2019 ioszhanghui@163.com. All rights reserved.
+//
+
+@import UIKit;
+#import "GMAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([GMAppDelegate class]));
+    }
+}
